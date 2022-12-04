9ab17495ff5c86c08ae865cc3d3b4b6bfdf78f87
flori-software <contact@flori-software.de>
2022-12-04 18:11:15 +0100

    Neue Klasse Plausi

---
+++ plausi.inc.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* Klasse mit Testmethoden, ob die offensichtlichen * Regeln für das Netzwerk erfüllt sind
+*/
+class Plausi {
+  public function namentest($wert) {
+    if (preg_match("/^\w{2,30}$/", $wert)) {
+        return 0;
+    } else {
+        return 1; 
+    }
+  }
+
+  public function emailtest($wert) {
+    $fehler = 0;
+    // Test notwendige E-Mail-Struktur
+    if (!preg_match("/\w+@\w+\.\w{2}/", $wert)) {
+        $fehler++; 
+    }
+    // nichtalphanumerische Zeichen – außer dem Zeichen @ 
+    if (preg_match("/\W/", $wert, $ergarray)) {
+        if ($ergarray[0] != "@")
+        $fehler++;
+    }
+    return $fehler;
+  }
+
+  public function nutzerdatentest($wert) {
+    $fehler = 0;
+    if (!preg_match("/^\w{8,20}$/", $wert)) {
+      $fehler++;
+    }
+    // Keine Zahl
+    if (!preg_match("/\d/", $wert)) {
+        $fehler++; }
+    // Kein Großbuchstabe
+    if (!preg_match("/[A-Z]/", $wert)) {
+        $fehler++; }
+    // Kein Kleinbuchstabe
+    if (!preg_match("/[a-z]/", $wert)) {
+        $fehler++; }
+    return $fehler;
+  } 
+}
+?>
--- registrieren.php
+++ registrieren.php
@@ -3,23 +3,83 @@
   <head>
     <meta charset="utf-8">
     <title>Image2Food –
-      Sag mir, was ich daraus kochen kann – Registrierung </
-title>
+      Sag mir, was ich daraus kochen kann – Registrierung 
+      </title>
 <meta name="viewport" content= "width=device-width, initial-scale=1.0">
   </head>
   <body>
     <div id="nav">
       <?php
       require("nav.php");
-?> </div>
+      require ("plausi.inc.php");
+      ?>
+ </div>
     <div id="content">
       <h1>Registrierung</h1>
       <?php
+      require ("registrieren.inc.php");
       /**
-* Image2Food
-* Das soziale, multimediale Netzwerk für Kochideen * Die Registrierungsseite
-*/
+      * Image2Food
+      * Das soziale, multimediale Netzwerk für Kochideen * Die Registrierungsseite
+      */
       class Registrierung {
+        /**
+         * Registrierungsmethode
+         * – Erst Eingaben des Anwenders plausibilisieren
+         * – Dann in der MySQL-Datenbank eintragen, wenn die Plausibilisierung keine Fehler ergeben hat
+        */
+        public function registrieren() {
+          if ($this -> plausibilisieren()) {
+            $this -> eintragen_db();
+          }
+    }
+        /**
+         * Plausibilisierungsmethode
+         * Testet die einzelnen Eingabefelder des
+         * Registrierungsformulars gegenüber
+         * – den Notwendigkeiten in der MySQL-Datenbank und
+         * – weiteren Anforderungen, die die Logik
+         * des Netzwerks fordert.
+         * Die Eingaben stehen im globalen Array $_POST
+         * zur Verfügung
+         * @return true, wenn die Plausibilisierung
+         * keine Fehler ergab – sonst false
+         */
+        private function plausibilisieren() {
+          // Fehlervariable
+          $anmelden = 0;
+          $p = new Plausi();
+          $anmelden += $p -> namentest($_POST['name']);
+          $anmelden += $p -> namentest($_POST['vorname']); 
+          $anmelden += $p -> emailtest($_POST['email']);
+          $anmelden += $p -> nutzerdatentest($_POST['userid']); 
+          $anmelden += $p -> nutzerdatentest($_POST['pw']);
+          // Kritische Zeichen auf der freien Eingabe
+          // der Zusatzinfos eliminieren
+          $_POST['zusatzinfos'] =
+            preg_replace("/[<|>|$|%|&|§]/", "#",
+              $_POST['zusatzinfos']);
+          // Testausgaben für den derzeitigen Stand
+          // des Projekts
+          echo "Die Eingaben: <hr>";
+          print_r($_POST);
+          echo "<br>Fehleranzahl: " . $anmelden . "<hr>";
+          if ($anmelden == 0) return true;
+          else return false;
+        }
+        /**
+         * Eintragen der Anmeldedaten in die Datenbank
+         * Die Eingaben stehen im globalen Array $_POST
+         * zur Verfügung
+         */
+        private function eintragen_db() {
+
+        }
+      }
+
+      $regobj = new Registrierung();
+      if (sizeof($_POST) > 0) {
+        $regobj -> registrieren();
       }
       ?>
     </div>
