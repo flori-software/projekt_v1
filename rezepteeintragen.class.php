3fac27fdcfac7f2827a35fe51a8dd11d178a94d7
flori-software <contact@flori-software.de>
2023-06-08 21:57:54 +0200

    Anfang Rezepte

--- rezepteeintragen.class.php
+++ rezepteeintragen.class.php
@@ -1,5 +1,20 @@
 <?php
+session_start();
+class RezeptEintragen {
+    function eintragen_db($bild, $antwort) {
+        @include("db.inc.php");
+        $userid = $_SESSION["name"];
+        if($stmt = $pdo->prepare("SELECT id_mitglied FROM mitgliederWHERE userid='$userid'")) {
+            $stmt->execute();
+            while($row = $stmt->fetch()) {
+                $id_antwortgeber = $row['id_mitglied'];
+                break;
+            }
+        }
 
+        if($stmt = $pdo->prepare("SELECT id_frage, id_mitglied FROM fragen WHERE bild='$bild'")) 
+    }
+}
 
 
 
