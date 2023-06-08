d1ce7e9db828164e597ce463ef5d564ca859ecef
flori-software <contact@flori-software.de>
2023-06-08 23:02:50 +0200

    Abschluss Praxisprojekt

--- rezepteeintragen.class.php
+++ rezepteeintragen.class.php
@@ -12,7 +12,34 @@ class RezeptEintragen {
             }
         }
 
-        if($stmt = $pdo->prepare("SELECT id_frage, id_mitglied FROM fragen WHERE bild='$bild'")) 
+        if($stmt = $pdo->prepare("SELECT id_frage, id_mitglied FROM fragen WHERE bild='$bild'")) {
+            $stmt->execute();
+            while($row->$stmt->fetch()) {
+                $id_frage        = $row["id_frage"];
+                $id_fragesteller = $row["id_mitglied"];
+                break;
+            }
+        }
+
+        if($stmt = $pdo.>prepare("INSERT INTO `antworten` (`id_fragesteller`, `id_frage`, `antwort`) VALUES (:id_fragesteller, :id_antwortgeber, :id_frage, :antwort)")) {
+            if($stmt->execute(array(
+                ':id_fragesteller' => $id_fragesteller,
+                ':id_antwortgeber' => $id_antwortgeber,
+                ':id_frage' => $id_frage,
+                ':antwort' => $antwort
+            ))) {
+                @include("wertupdate.php");
+                new WertUpdate("antworten", $id_antwortgeber);
+                echo 'Ihr Rezeptvorschlag wurde eingetragen.';
+            }
+        }
+    }
+}
+
+if(isset($_GET["bild"]) && isset($_GET["rezeptvorschlag"])) {
+    if(strlen($_GET["rezeptvorschlag"])) {
+        $obj = new RezeptEintragen();
+        $obj->eintragen_db($_GET["bild"], $_GET["rezeptvorschlag"]);
     }
 }
 
